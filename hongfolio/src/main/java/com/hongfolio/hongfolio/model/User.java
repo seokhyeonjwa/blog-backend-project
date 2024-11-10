61663a8012ee85e936925d67b1001d5f89dd6847
hong2 <ghdtjrgus6@naver.com>
2024-11-11 04:38:21 +0900

    frontend연동 중

--- hongfolio/src/main/java/com/hongfolio/hongfolio/model/User.java
+++ hongfolio/src/main/java/com/hongfolio/hongfolio/model/User.java
@@ -6,12 +6,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -24,6 +28,15 @@ public class User {
 
     @NotBlank
     private String password;
+
+    @NotBlank
+    private String nickname;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String phoneNumber;
 
     private String role = "USER"; // default 사용자 역할
 
